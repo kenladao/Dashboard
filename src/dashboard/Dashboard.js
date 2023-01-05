417e37d34a1ee87083921923e15fc67f9f44a03e
kennedy.l <kennedy.l@ltlabs.co>
2023-01-05 14:39:47 +0700

    monthly sale

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -1,15 +1,64 @@
 import React from 'react';
 import Grid from '@mui/material/Grid';
-import {data} from './data/data';
+import { data } from './data/data';
 import { Paper, Typography } from '@mui/material';
 
 import { useStyles } from './DashboardMui';
 import "./Dashboard.css"
-
-
+// import { Line } from 'react-chartjs-2';
 
 const Dashboard = () => {
     const classes = useStyles();
+
+    // const Graphdata = {
+    //     labels: [
+    //     "Ambient",
+    //     "Fresh",
+    //     "Frozen",
+    //     "Fruit&Veg"
+    //     ],
+    //     datasets: [
+    //       {
+    //         label: "",
+    //         backgroundColor: "#D9D9D9",
+    //         borderColor: "#D9D9D9",
+    //         hoverBackgroundColor: "#D9D9D9",
+    //         hoverBorderColor: "#D9D9D9",
+    //         data: [158, 105, 78, 360],
+    //         barPercentage: 0.75,
+    //         categoryPercentage: 0.5
+    //       },
+    //     ]
+    //   };
+      
+      const options = {
+        maintainAspectRatio: false,
+        cornerRadius: 2,
+        legend: {
+          display: false
+        },
+        scales: {
+          yAxes: [
+            {
+              gridLines: {
+                display: false
+              },
+              stacked: false,
+              ticks: {
+                stepSize: 20
+              }
+            }
+          ],
+          xAxes: [
+            {
+              stacked: false,
+              gridLines: {
+                color: "transparent"
+              }
+            }
+          ]
+        }
+      };
     return (
         <div className="Dashboard">
             <Grid
@@ -20,70 +69,81 @@ const Dashboard = () => {
                 spacing={1}
             >
                 <Grid item xs={4}>
-                    <div className="graph">
-                        <span className="one">
-                        Total Sales by category
-                        </span>
-                    </div>
+                    <Paper className={classes.graph}>
+                        <Typography className={classes.graphHeader}>
+                            Total Sales by category
+                        </Typography>
+                        {/* <Line Graphdata={Graphdata} options={options} /> */}
+                    </Paper>
                 </Grid>
                 <Grid item xs={8}>
-                <Grid  container
-                direction="row"
-                justifyContent="space-evenly"
-                alignItems="flex-start"
-                spacing={3}>
-                <Grid item xs={3} style={{position: "center"}}>
-                        <Paper className={classes.paper}>
-                        <Typography className={classes.headerTwo}>
-                            Total Sales this month
-                        </Typography>
-                        <Typography className={classes.bodytwo}>
-                            120
-                        </Typography>
-                        </Paper>
-                </Grid>
-                <Grid item xs={6}>
-                    <div className='fresh'>
-                    Fresh
-                    </div>
-                </Grid>
-                <Grid item xs={3}>
-                    <div className='nameOne'>
-                    Name One
-                    </div>
-                </Grid>
-                <Grid item xs={5}>
-                    <div className='bestSeller'>
-                    best selling product
-                    </div>
-                </Grid>
-                <Grid item xs={5}>
-                    <div className='totalMargin'>
-                    total Margin
-                    </div>
-                </Grid>
-                        <Grid item xs={2}>
-                            <div className='monthly'>
-                                <span className='monthlySale'>Monthly Sales</span>
-                            {data?.map((item) => (
-                                <Grid container
-                                    direction="row"
-                                    justifyContent="space-between"
-                                    alignItems="center">
-                                    <Grid item xs={6} >
-                                        <span className="date">
-                                        {item?.date}
-                                        </span>
-                                    </Grid>
-                                    <Grid item xs={6} >
-                                        <span className="sale">
-                                        {item?.sale}
-                                        </span>
-                                    </Grid>
-                                </Grid>))}
+                    <Grid container
+                        direction="row"
+                        justifyContent="space-evenly"
+                        alignItems="flex-start"
+                        spacing={3}>
+                        <Grid item xs={3} style={{ position: "center" }}>
+                            <Paper className={classes.paper}>
+                                <Typography className={classes.headerTwo}>
+                                    Total Sales this month
+                                </Typography>
+                                <Typography className={classes.bodytwo}>
+                                    120
+                                </Typography>
+                            </Paper>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Paper className={classes.paper1}>
+                                <Typography className={classes.header3}>
+                                    Total Sales this month
+                                </Typography>
+                                <Typography className={classes.body3}>
+                                    Fresh
+                                </Typography>
+                            </Paper>
+                        </Grid>
+                        <Grid item xs={3}>
+                            <Paper className={classes.paper2}>
+                                <Typography className={classes.header4}>
+                                    Total Sales this month
+                                </Typography>
+                                <Typography className={classes.body4}>
+                                    Name One
+                                </Typography>
+                            </Paper>
+                        </Grid>
+                        <Grid item xs={5}>
+                            <div className='bestSeller'>
+                                best selling product
                             </div>
                         </Grid>
-                </Grid>
+                        <Grid item xs={5}>
+                            <div className='totalMargin'>
+                                total Margin
+                            </div>
+                        </Grid>
+                        <Grid item xs={2}>
+                            <Paper className={classes.monthly}>
+                                <Typography className={classes.monthHeader}>Monthly Sales</Typography>
+                                {data?.map((item) => (
+                                    <Grid container
+                                        direction="row"
+                                        justifyContent="space-between"
+                                        alignItems="center">
+                                        <Grid item xs={6} >
+                                            <Typography className={classes.date}>
+                                                {item?.date}
+                                            </Typography>
+                                        </Grid>
+                                        <Grid item xs={6} >
+                                            <Typography className={classes.sale}>
+                                                {item?.sale}
+                                            </Typography>
+                                        </Grid>
+                                    </Grid>))}
+                            </Paper>
+                        </Grid>
+                    </Grid>
                 </Grid>
                 <Grid item xs={12}>
                     <Grid container
@@ -98,7 +158,7 @@ const Dashboard = () => {
                             </div>
                         </Grid>
                         <Grid item xs={2}>
-                            <Grid container  direction="column" spacing={2}>
+                            <Grid container direction="column" spacing={2}>
                                 <Grid item xs={2}>
                                     <div className='mostSale'>
                                         Sales Person per this month
@@ -111,7 +171,7 @@ const Dashboard = () => {
                                 </Grid>
                             </Grid>
                         </Grid>
-                </Grid>
+                    </Grid>
                 </Grid>
             </Grid>
         </div>);
--- src/dashboard/DashboardMui.js
+++ src/dashboard/DashboardMui.js
@@ -1,6 +1,23 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export const useStyles = makeStyles((theme) => ({
+    graph:{
+        width: "auto",
+        height: "350px",
+        background: "#3D3D3D",
+        borderRadius: "20px",
+        textAlign: "start",
+    },
+    graphHeader:{
+        fontFamily: 'Inter',
+        fontStyle: 'normal',
+        fontWeight: 700,
+        fontSize: 16,
+        alignItems: 'center',
+        color: '#40F2DD',
+        paddingLeft: 27,
+        paddingTop: 14
+    },
     paper: {
         width: "auto",
         height: "148px",
@@ -27,5 +44,98 @@ export const useStyles = makeStyles((theme) => ({
         textAlign: 'center',
         color: "#40F2DD",
         marginTop: 12
+    },
+
+    paper1: {
+        width: "auto",
+        height: "165px",
+
+        backgroundColor: "#D9D9D9",
+        borderRadius: "20px",
+        
+    },
+    header3: {
+        fontFamily: 'Inter',
+        fontStyle: 'normal',
+        fontWeight: 700,
+        fontSize: 16,
+        alignItems: 'center',
+        textAlign: 'center',
+        color: '#FFFFFF',
+        padding: "10px 5px 10px 5px"
+
+    },
+    body3: {
+        fontFamily: 'Inter',
+        fontStyle: 'normal',
+        fontWeight: 700,
+        fontSize: 40,
+        alignItems: 'center',
+        textAlign: 'center',
+        color: "#000000",
+        marginTop: 12,
+        padding: "10px 5px 10px 5px"
+    },
+    paper2: {
+        width: "auto",
+        height: "165px",
+
+        backgroundColor: "#40F2DD",
+        borderRadius: "20px",
+        
+    },
+    header4: {
+        fontFamily: 'Inter',
+        fontStyle: 'normal',
+        fontWeight: 700,
+        fontSize: 16,
+        alignItems: 'center',
+        textAlign: 'center',
+        color: '#000000',
+        padding: "10px 5px 10px 5px"
+
+    },
+    body4: {
+        fontFamily: 'Inter',
+        fontStyle: 'normal',
+        fontWeight: 700,
+        fontSize: 30,
+        alignItems: 'center',
+        textAlign: 'center',
+        color: "#FFFFFF",
+        padding: "10px 5px 10px 5px"
+    },
+
+    monthly:{
+        height: 165,
+        background: "#3D3D3D",
+        borderRadius: 20
+    },
+    monthHeader: {
+        fontFamily: 'Inter',
+        fontStyle: 'normal',
+        fontWeight: 700,
+        fontSize: 16,
+        alignItems: 'center',
+        textAlign: 'center',
+        color: '#000000',
+        padding: "2px"
+    },
+    date:{
+        fontFamily: 'Inter',
+        fontStyle: "normal",
+        fontWeight: 700,
+        fontSize: "10px",
+        alignItems: "center",
+        textAlign: 'left'
+    },
+    sale: {
+        fontFamily: 'Inter',
+        fontStyle: "normal",
+        fontWeight: 700,
+        fontSize: "10px",
+        alignItems: "center",
+        textAlign: 'right',
+        color: "#40F2DD"
     }
 }));
